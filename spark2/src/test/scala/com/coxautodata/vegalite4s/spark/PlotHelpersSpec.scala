845f06ea8b999b4fbadd907f620c2c8c5d3ab1eb
Alex Bush <6716812+alexjbush@users.noreply.github.com>
2019-03-24 13:01:59 +0000

    Updated default version of VegaLite (#6)

--- spark2/src/test/scala/com/coxautodata/vegalite4s/spark/PlotHelpersSpec.scala
+++ spark2/src/test/scala/com/coxautodata/vegalite4s/spark/PlotHelpersSpec.scala
@@ -32,7 +32,7 @@ class PlotHelpersSpec extends FunSpec with Matchers {
 
     plot.toJson should be(
       """{
-        |  "$schema" : "https://vega.github.io/schema/vega-lite/v2.json",
+        |  "$schema" : "https://vega.github.io/schema/vega-lite/v3.json",
         |  "description" : "A simple bar chart with embedded data.",
         |  "mark" : "bar",
         |  "encoding" : {
@@ -196,7 +196,7 @@ class PlotHelpersSpec extends FunSpec with Matchers {
 
     plot.toJson should be(
       """{
-        |  "$schema" : "https://vega.github.io/schema/vega-lite/v2.json",
+        |  "$schema" : "https://vega.github.io/schema/vega-lite/v3.json",
         |  "description" : "A simple bar chart with embedded data.",
         |  "mark" : "bar",
         |  "encoding" : {
